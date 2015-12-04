739c2ef6f85f5c8db4362029ae20d8a0191b41d7
TracySP <414115470@qq.com>
2015-12-04 14:25:53 +0800

    sp
    111

---
+++ ReadingToday/ReadingToday/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ReadingToday
+//
+//  Created by 低调的小宋 on 15/12/4.
+//  Copyright © 2015年 HShandsome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ReadingToday/ReadingToday/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ReadingToday
+//
+//  Created by 低调的小宋 on 15/12/4.
+//  Copyright © 2015年 HShandsome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
